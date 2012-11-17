e90fd622ed23881ac6452e5cc483f8443d9a6ce6
Brandon Forster <brandonforster@gmail.com>
2012-11-17 16:14:02 -0500

    added rule for gitignore to ignore text files to ignore test cases. added code to scanner that will launch other classes.

--- src/Scanner.java
+++ src/Scanner.java
@@ -6,9 +6,43 @@
 //Scanner and Helper Object
 
 public class Scanner {
-
-	public static void main(String[] args) {
+	
+	private String inputFileText;
+	
+	public Scanner(String inputFilename)
+	{
 		
+	}
+
+	/**
+	 * The main function for @Scanner.
+	 * 
+	 * Functions as a helper function to launch the rest of the program. 
+	 * 
+	 * @param args 0 must be "encode" or "decode", 1 is the filename 
+	 */
+	public static void main(String[] args)
+	{
+		//perform an encode
+		if (args[0].compareTo("encode") == 0)
+		{
+			new Scanner(args[1]);
+		}
+		
+		//perform a decode
+		if (args[0].compareTo("decode") == 0)
+		{
+			new Scanner(args[1]);
+		}
+		
+		//if there is an error in the scripting file
+		else
+		{
+			System.out.println("Invalid argument to function call.\n" +
+					"Please edit your argument.");
+			System.exit(1);
+			
+		}
 	}
 
 }
