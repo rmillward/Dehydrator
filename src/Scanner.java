f1ea49e04824726bce5be67253a0ac12a32c98fa
Brandon Forster <brandonforster@gmail.com>
2012-11-17 14:59:52 -0500

    Added readme. Added assignment file. Added skeleton for scanner/helper

---
+++ src/Scanner.java
@@ -0,0 +1,14 @@
+//Brandon Forster, Robert Millward,
+//William Corlett and Phillip Rajala
+//COP 3503 Assignment 4
+//Huffman Encoding and Decoding
+//21 November 2012
+//Scanner and Helper Object
+
+public class Scanner {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
