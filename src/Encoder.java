4999fd2d302a41f2cfc118dd48bb57d7c31cc769
Brandon Forster <brandonforster@gmail.com>
2012-11-24 14:50:12 -0500

    forms a frequency table. does not do anything with this information.

--- src/Encoder.java
+++ src/Encoder.java
@@ -16,6 +16,18 @@ public class Encoder {
 	 * @param text text to be encoded as an argument
 	 */
 	public Encoder(String text) {
+		
+		//make a table to keep track of how many times each ascii character
+		//appears
+		int[] frequencyTable= new int[ASCII_ALPHABET_SIZE];
+		
+		//for each character in text
+		for (int i = 0; i< text.length(); i++)
+		{
+			//makes an int cast on the character at i, giving us its ascii
+			//value. increment that ascii value in the table
+			frequencyTable[(int)text.charAt(i)]++;
+		}
 	}
 
 	//return the compression ratio of the original file:compressed file
