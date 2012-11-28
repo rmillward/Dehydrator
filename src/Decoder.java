200d6f7a2f023dee17610b9cc5f28bac94751e1d
rmillward <meleechampion@gmail.com>
2012-11-28 00:42:14 -0500

    added getTree

--- src/Decoder.java
+++ src/Decoder.java
@@ -47,14 +47,51 @@ public class Decoder {
 		}
 	}
 	
-	public Node getTree(Node root)
+	public Node getTree(Node root, String text)
 	{
-		if (root.isLeaf())
-			return root;
-		else
-		{
-			return new Node(this.get, 0, root, root)
+		// parse out our tree from the giant String
+		String textWeWant = text.substring(0, text.indexOf(DELIMITER));
+		String[] tokens = textWeWant.split(" ");
+		
+		// fill out our tree
+		String path = 0;
+		int id, freq = 0;
+		Node current, next;
+		current = root;
+		
+		for (int i=0; i < tokens.length; i+=3){
+			// parsing relevant info
+			path = tokens[i];
+			id = tokens[i+1];
+			freq = tokens[i+2];
+			
+			// follwing the path
+			for (j=0; j < path.length-1; j++){
+				if (path.charat(j) == 0){
+					next = current.left;
+					if (next == null){ // if a child doesn't exist, create an empty one
+						next = new Node((char)-1, -1, null, null);
+						current.left = next; // assign the newborn child
+					}
+				}
+				else if (path.charat(j) == 1){
+					next = current.right;
+					if (next == null){
+						next = new Node((char)-1, -1, null, null);
+						current.right = next;
+					}
+				}
+				current = next;
+				next = null;
+			}
+			
+			// setting variables at the end of that path
+			current.id = id;
+			current.freq = freq;
+			current = root;
 		}
+		
+		return root;
 	}
 
 	public String getText()
